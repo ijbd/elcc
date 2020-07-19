8f6c6d9cbc37929db81baa4a15142519a709c164
ijbd <ijbd@umich.edu>
2020-07-19 11:33:03 -0600

    pace 2018 w/ storage.

--- src/elcc.py
+++ src/elcc.py
@@ -2,7 +2,6 @@ import os
 import sys
 
 root_directory = '/scratch/mtcraig_root/mtcraig1/shared_data/elccJobs/' + sys.argv[1]
-RAM = sys.argv[2] #4GB, 8GB (8GB will send email; used for saving systems)
 
 def error_handling():
 
@@ -20,14 +19,36 @@ def error_handling():
 def add_job(parameters):
 
     global root_directory
+    global num_jobs
 
-    parameter_string = root_directory
+    num_jobs += 1
+    parameter_string = str(job)+' '+root_directory
 
     for key in parameters:
         parameter_string = parameter_string + ' ' + str(key) + ' ' + str(parameters[key])
     
     with open('elcc_job.txt','a') as f:
-        f.write('python -u elcc_master.py ' + parameter_string + '\n')
+        #f.write('sbatch elcc_batch_job.sbat ' + parameter_string + '\n')
+        f.write('bash elcc_batch_job.sh' + parameter_string + '\n')
+
+def run_jobs(jobs_per_batch):
+    i = 0
+    jobs_completed = 0
+
+    if not os.path.exists('elcc_job.txt'):
+        sys.exit(1)
+
+    with open('elcc_job.txt','r') as f:
+        for line in f:
+            if jobs_completed >= jobs_per_batch:
+                sys.exit(0)
+            if i == job:
+                os.system(line)
+                job += 1
+                jobs_completed += 1
+            i += 1
+    return        
+
 
 def main():
 
@@ -43,13 +64,20 @@ def main():
     parameters['generator_storage'] = True
 
     # variable parameters
-    add_job(parameters)
+    for year in [2016, 2017, 2018]:
+        parameters['year'] = year
+        add_job(parameters)
 
-    os.system('sbatch elcc_batch_job_'+RAM+'.sbat')
+    print('Job list created')
 
-    print('Jobs submitted')
+    # run jobs
 
+    job = 0 
+    if len(sys.argv) == 3:
+        job = int(sys.argv[2])
 
+    if job >= num_jobs:
+        sys.exit()
 
 if __name__ == "__main__":
     error_handling()
