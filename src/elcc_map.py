ca417edc31258d89d1cfe5b18142043e00029176
ijbd <ijbd@umich.edu>
2020-08-05 13:44:53 -0600

    Mountains 1 GW solar 500 MW 4 Hour storage.

--- src/elcc_map.py
+++ src/elcc_map.py
@@ -108,7 +108,7 @@ def main():
     parameters['generator type'] = 'solar'
     parameters['generator storage'] = 'True'
     parameters['generator storage power capacity'] = 500
-    parameters['generator storage energy capacity'] = 500
+    parameters['generator storage energy capacity'] = 2000
     
     # variable parameters
     solar_cf_file = "../wecc_powGen/2018_solar_generation_cf.nc" # only used for getting lat/lons
