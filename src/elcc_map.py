90959816235975a7b38424dffc012e4df2b2c756
ijbd <ijbd@umich.edu>
2020-08-11 13:11:40 -0600

    California 5 GW solar.

--- src/elcc_map.py
+++ src/elcc_map.py
@@ -101,14 +101,14 @@ def main():
 
     # universal parameters
     parameters['year'] = 2016
-    parameters['region'] = 'Basin'
+    parameters['region'] = 'California'
     parameters['iterations'] = 5000
 
-    parameters['nameplate'] = 1000
-    parameters['generator type'] = 'wind'
-    parameters['generator storage'] = 'True'
-    parameters['generator storage power capacity'] = 500
-    parameters['generator storage energy capacity'] = 500
+    parameters['nameplate'] = 5000
+    parameters['generator type'] = 'solar'
+    #parameters['generator storage'] = 'True'
+    #parameters['generator storage power capacity'] = 500
+    #parameters['generator storage energy capacity'] = 500
     
     # variable parameters
     solar_cf_file = "../wecc_powGen/2018_solar_generation_cf.nc" # only used for getting lat/lons
