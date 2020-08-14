2facbaf2c16ba5a856703c288bbfdf560f8b401f
ijbd <ijbd@umich.edu>
2020-08-14 16:11:41 -0600

    scale renewable map.

--- src/elcc_map.py
+++ src/elcc_map.py
@@ -101,11 +101,11 @@ def main():
 
     # universal parameters
     parameters['year'] = 2016
-    parameters['region'] = 'Basin'
+    parameters['region'] = 'Northwest'
     parameters['iterations'] = 5000
 
     parameters['nameplate'] = 1000
-    parameters['generator type'] = 'wind'
+    parameters['generator type'] = 'solar'
     parameters['renewable multiplier'] = 2
     
     # variable parameters
