f76b66990c43e86f4d58f855dcf5adf2a4fc3770
ijbd <ijbd@umich.edu>
2020-08-21 11:29:13 -0600

    5x Renewables.

--- src/elcc_map.py
+++ src/elcc_map.py
@@ -101,9 +101,9 @@ def main():
 
     # universal parameters
     parameters['year'] = 2016
-    parameters['region'] = 'California'
+    parameters['region'] = 'Mountains'
     parameters['iterations'] = 5000
-    parameters['renewable multiplier'] = 2
+    parameters['renewable multiplier'] = 5
 
     parameters['nameplate'] = 1000
     parameters['generator type'] = 'solar'
---
+++ src/fleet_compositions.py
@@ -0,0 +1,146 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+from elcc_impl import get_powGen, get_solar_and_wind_fleet
+
+def get_conventional_fleet_impl(plants,active_generators,year):
+# Filter generators for operation status, technology, year of simulation
+
+    # filtering
+    active_generators = active_generators[(active_generators["Operating Year"] <= year)]
+    active_generators = active_generators[(active_generators["Status"] == "OP")]
+    active_generators = active_generators[(~active_generators["Technology"].isin(["Solar Photovoltaic", "Onshore Wind Turbine", "Offshore Wind Turbine", "Batteries"]))]
+    
+    # Fill empty summer/winter capacities
+    active_generators["Summer Capacity (MW)"].where(active_generators["Summer Capacity (MW)"].astype(str) != " ",
+                                                    active_generators["Nameplate Capacity (MW)"], inplace=True)
+    active_generators["Winter Capacity (MW)"].where(active_generators["Winter Capacity (MW)"].astype(str) != " ", 
+                                                    active_generators["Nameplate Capacity (MW)"], inplace=True)
+    
+    # Convert Dataframe to Dictionary of numpy arrays
+    conventional_generators = dict()
+    conventional_generators["num units"] = active_generators["Nameplate Capacity (MW)"].values.size
+    conventional_generators["nameplate"] = active_generators["Nameplate Capacity (MW)"].values
+    conventional_generators["summer nameplate"] = active_generators["Summer Capacity (MW)"].values
+    conventional_generators["winter nameplate"] = active_generators["Winter Capacity (MW)"].values
+    conventional_generators["year"] = active_generators["Operating Year"].values
+    conventional_generators["technology"] = active_generators["Technology"].values
+
+    if conventional_generators["nameplate"].size == 0:
+        error_message = "No existing conventional found."
+        raise RuntimeError(error_message)
+    
+    return conventional_generators
+
+def get_conventional_fleet(eia_folder, regions, year):
+    """ Retrieve all active conventional generators
+
+    ...
+
+    Args:
+    ----------
+    `eia_folder` (string): file path to Form EIA-860 Folder
+
+    `regions` (str): balancing authority
+
+    `year` (int): year of interest
+
+    `system_preferences` (dict): set of parameters. Must include keys 'temperature dependent FOR', 'temperature dependent FOR indpendent of size', and 'conventional efor'
+    
+    `powGen_lats` (ndarray): vector of latitudes to map temperature data to
+
+    `powGen_lons` (ndarray): vector of longitudes to map temperature data to
+    
+    `temperature_data` (ndarray): array of temperatures in Celsius to map generators to. Dimensions correspond to 'powGen_lats' and 'powGen_lons'
+
+    `benchmark_fors` (ndarray): array of temperature-dependent FORs for different generator technologies
+
+    """
+
+    # Open files
+    plants = pd.read_excel(eia_folder+"2___Plant_Y"+str(year)+".xlsx",skiprows=1,usecols=["Plant Code","NERC Region","Latitude",
+                                                                                "Longitude","Balancing Authority Code"])
+    all_conventional_generators = pd.read_excel(eia_folder+"3_1_Generator_Y"+str(year)+".xlsx",skiprows=1,\
+                                                    usecols=["Plant Code","Generator ID","Technology","Nameplate Capacity (MW)","Status",
+                                                            "Operating Year", "Summer Capacity (MW)", "Winter Capacity (MW)"])
+    # Sort by NERC Region and Balancing Authority to filter correct plant codes
+    nerc_region_plant_codes = plants["Plant Code"][plants["NERC Region"].isin(regions)].values
+    balancing_authority_plant_codes = plants["Plant Code"][plants["Balancing Authority Code"].isin(regions)].values
+    
+    desired_plant_codes = np.concatenate((nerc_region_plant_codes, balancing_authority_plant_codes))
+
+    # Error Handling
+    if desired_plant_codes.size == 0:
+        error_message = "Invalid region(s): " + str(regions)
+        raise RuntimeError(error_message)
+
+    # Get operating generators
+    active_generators = all_conventional_generators[(all_conventional_generators["Plant Code"].isin(desired_plant_codes))]
+
+    return get_conventional_fleet_impl(plants,active_generators,year)
+
+def main():
+    year = 2018
+    regions = ['Basin','California','Mountains','Northwest','Southwest']
+    eia_folder = '../eia8602018/'
+
+    TEPPC_regions = pd.read_csv('../demand/_regions.csv').fillna('nan')
+
+    fleet = pd.DataFrame(columns=regions,index=['Conventional (MW)','Solar (MW)','Wind (MW)','Total (MW)','Conventional (%)','Solar (%)','Wind (%)'])
+    
+    # powGen
+    powGen_lats, powGen_lons, cf = get_powGen('../wecc_powGen/2016_solar_generation_cf.nc','../wecc_powGen/2016_solar_generation_cf.nc')
+
+    # map setup
+    fig, ((solar_ax, wind_ax), (solar_idx_ax, wind_idx_ax))= plt.subplots(nrows=2, ncols=2,figsize=(20,15))
+
+    for region in regions:
+
+        all_regions = np.array([TEPPC_regions[region].values.flatten().astype(str)])
+        all_regions = np.unique(all_regions[all_regions != 'nan'])
+
+        conv = get_conventional_fleet(eia_folder,all_regions,year)
+        solar, wind = get_solar_and_wind_fleet(eia_folder, all_regions, year, 1, powGen_lats, powGen_lons, 1)
+        c = np.sum(conv['nameplate'])
+        s = np.sum(solar['nameplate'])
+        w = np.sum(wind['nameplate'])
+        t = c+s+w
+        fleet.at['Conventional (MW)',region] = round(c)
+        fleet.at['Solar (MW)',region] = round(s)
+        fleet.at['Wind (MW)',region] = round(w)
+        fleet.at['Total (MW)',region] = round(t)
+        fleet.at['Conventional (%)',region] = round(c/t*100)
+        fleet.at['Solar (%)',region] = round(s/t*100)
+        fleet.at['Wind (%)',region] = round(w/t*100)
+
+        solar_ax.scatter(solar['lon'],solar['lat'])
+        wind_ax.scatter(wind['lon'],wind['lat'])
+
+        solar_idx_ax.scatter(powGen_lons[solar['lon idx']],powGen_lats[solar['lat idx']])
+        wind_idx_ax.scatter(powGen_lons[wind['lon idx']],powGen_lats[wind['lat idx']])
+
+    for ax in [solar_ax, wind_ax, solar_idx_ax, wind_idx_ax]:
+        ax.legend(regions)
+        ax.set_xlabel('Longitude')
+        ax.set_ylabel('Latitude')
+
+    solar_ax.set_title('Solar Plant Locations')
+    wind_ax.set_title('Wind Plant Locations')
+    solar_idx_ax.set_title('Solar Plant Closest CF Locations')
+    wind_idx_ax.set_title('Wind Plant Closest CF Locations')
+
+    # wrap up
+
+    plt.savefig('regional_composition_map')
+
+    fleet.to_csv('regional_composition.csv')
+
+
+    
+
+
+    print('All Finished!')
+
+
+if __name__ == "__main__":
+    main()
