4b003533bea700105d080e12948ddea3e6a7e675
ijbd <ijbd@umich.edu>
2020-08-21 10:47:54 -0600

    5x Renewables.

--- src/elcc.py
+++ src/elcc.py
@@ -91,6 +91,8 @@ def fix_region_string(parameters):
 
 def main():
 
+    global root_directory
+
     parameters = dict()
     parameters['root directory'] = root_directory
 
@@ -98,19 +100,34 @@ def main():
 
     # universal parameters
     parameters['year'] = 2016
-    parameters['region'] = 'Mountains'
     parameters['iterations'] = 5000
-    parameters['renewable multiplier'] = 1
+    parameters['renewable multiplier'] = 5
 
-    parameters['nameplate'] = 1000
-    parameters['generator type'] = 'solar'
-    parameters['latitude'] = 36.0
-    parameters['longitude'] = -113.125
-    parameters['debug'] = 'True'
-    
+    # 5x Renewables for all
+    for region in ['basin','california','mountains','northwest','southwest']:
 
-    add_job(parameters)
-    run_job()
+        #finagle
+        root_directory = '../../elccJobs/'+region+'/'+str(parameters['year'])+'/'
+        parameters['root directory'] = root_directory
+
+        #change region
+        parameters['region'] = region.capitalize()
+        add_job(parameters)
+        run_job()
+
+    # 1.5x and 3x for Cal
+    parameters['region'] = 'California'
+    root_directory = '../../elccJobs/california/2016/'
+    parameters['root directory'] = root_directory
+
+    for multiplier in [1.5, 3]:
+        parameters['renewable multiplier'] = multiplier
+        add_job(parameters)
+        run_job()
+
+
+
+
 
 
 
